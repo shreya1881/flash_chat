7ec6739890f8b4bb927b55e26a7de64bf7e18586
shreya1881 <2704shreya@gmail.com>
2022-12-25 15:43:56 +0530

    Updated version of project

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -41,7 +41,7 @@ class _ChatScreenState extends State<ChatScreen> {
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
-                  FlatButton(
+                  TextButton(
                     onPressed: () {
                       //Implement send functionality.
                     },
